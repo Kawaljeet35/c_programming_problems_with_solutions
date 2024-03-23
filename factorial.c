8562f1d9df9b85314d19c2588b614cc9308b7fcb
Kawaljeet Singh <123053797+Kawaljeet35@users.noreply.github.com>
2024-03-23 10:43:18 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -6,6 +6,31 @@ and return its factorial.
 
 Example Input/Output:
 
+Enter the number: 3
+The factorial of 3 is: 6
+
+Enter the number: 6
+The factorial of 6 is: 720
 */
 
 //Solution:
+#include <stdio.h>
+
+int factorial(int n);
+
+int main() {
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    int result = factorial(n);
+    printf("The factorial of %d is: %d\n", n, result);
+    return 0;
+}
+
+int factorial(int n) {
+    if (n <= 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
