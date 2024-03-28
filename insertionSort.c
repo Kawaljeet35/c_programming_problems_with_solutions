11019bc64e1c3d6f6b37ecc8650d88ffc294cc41
Kawaljeet Singh <123053797+Kawaljeet35@users.noreply.github.com>
2024-03-28 21:43:25 +0530

    Create insertionSort.c

---
+++ insertionSort.c
@@ -0,0 +1,40 @@
+/*
+Insertion Sort
+
+Write a program in C to implement the Insertion Sort algorithm and 
+sort a given list in ascending order.
+*/
+
+//Solution:
+#include <stdio.h>
+
+void insertionSort(int array[], int n) {
+    int i, j, current_element;
+    
+    for (i = 1; i < n; i++) {
+        current_element = array[i];
+        j = i;
+        
+        while (j > 0 && array[j - 1] > current_element) {
+            array[j] = array[j - 1];
+            j--;
+        }
+        
+        array[j] = current_element;
+    }
+}
+
+int main() {
+    int array[] = {8, 3, 7, 4, 1};
+    int n = sizeof(array) / sizeof(array[0]);
+
+    insertionSort(array, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
